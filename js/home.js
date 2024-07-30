cd53d5059fc2b18ae7d0bdd9ca7775cb2759d6bd
Brad Young <bradyoung109@gmail.com>
2024-07-30 21:26:11 +1200

    Portfolio version 3D

---
+++ js/home.js
@@ -0,0 +1,68 @@
+// Banner button scroll
+// var scrollBtn = document.querySelector('#down-arrow');
+// // var scrollBtnBottom = document.querySelector('#scroll-btn-bottom');
+
+// function scrollDown(){
+//     document.querySelector('#projects').scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+// }
+
+// scrollBtn.addEventListener('click', function(){
+//     scrollDown();
+// });
+
+// Banner button scroll
+var scrollBtn = document.querySelector('#scroll-btn');
+// var scrollBtnBottom = document.querySelector('#scroll-btn-bottom');
+
+function scrollDown(){
+    document.querySelector('#projects').scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+}
+
+scrollBtn.addEventListener('click', function(){
+    scrollDown();
+});
+
+
+// Change colour of side icons
+let sideLinks = document.querySelectorAll('.side-icon')
+
+lightMode = () => {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        for(const icon of sideLinks){
+            icon.classList.add('light-icon');
+        }
+      } else {
+        for(const icon of sideLinks){
+            icon.classList.remove('light-icon');
+        }
+    }
+    // console.log(sideLinks)
+}
+
+window.addEventListener('scroll', lightMode)
+
+// Change colour of side icons
+let sideLinkLineLong = document.querySelector('.long-line')
+let sideLinkLineShort = document.querySelector('.short-line')
+
+showLines = () => {
+    if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+        sideLinkLineLong.classList.add('show-long-line');
+        sideLinkLineShort.classList.add('show-short-line');
+      } else {
+        sideLinkLineLong.classList.remove('show-long-line');
+        sideLinkLineShort.classList.remove('show-short-line');
+    }
+}
+
+window.addEventListener('scroll', showLines)
+
+
+// Horizontal archive scroll
+// let scrollContainer = document.querySelector(".archive-container");
+
+// scrollContainer.addEventListener("wheel", (evt) => {
+//     evt.preventDefault();
+//     scrollContainer.scrollLeft += evt.deltaY;
+// });
+  
---
+++ js/index.js
@@ -0,0 +1,114 @@
+// // ==================================================================
+// // VARIABLES
+// // ==================================================================
+
+// let home = {
+// 	init: {
+// 		attachListeners: undefined,
+// 		init: undefined,
+// 	},
+
+// 	elem: {
+//         scrollBtn: document.querySelector('#down-arrow'),
+//         scrollBtnBottom: document.querySelector('#up-arrow'),
+//         about: document.querySelector('.about'),
+//         portfolioPreview: document.querySelector('.portfolio'),
+// 		nav: document.querySelector('.nav'),
+// 		// talkBtn: document.querySelector('.lets-talk-btn'),
+// 		contact: document.querySelector('#contact')
+// 	},
+
+// 	event: {
+//         // Scroll Buttons
+// 		scrollDown: undefined,
+// 		scrollUp: undefined,
+// 		showNav: undefined,
+// 		scrollTalk: undefined
+// 	},
+// };
+
+// // ==================================================================
+// // FUNCTIONS
+// // ==================================================================
+
+// // @func  home.initialise
+// // @desc
+// home.init.init = () => {
+// 	home.init.attachListeners();
+// 	// home.sections.forEach((section) => {
+// 	// 	home.sectionPos.push(section.offsetTop);
+// 	// });
+// };
+
+// home.init.attachListeners = () => {
+//     home.elem.scrollBtn.addEventListener("click", home.event.scrollDown);
+//     // home.elem.scrollBtnBottom.addEventListener("click", home.event.scrollUp);
+// 	window.addEventListener("scroll", home.event.showNav);
+// 	// home.elem.talkBtn.addEventListener("click", home.event.scrollTalk);
+// };
+
+// home.event.scrollDown = () => {
+//     home.elem.about.scrollIntoView({behavior: "smooth", block: "center"});
+// }
+// home.event.scrollUp = () => {
+//     window.scrollTo({top: 0, behavior: 'smooth'});
+// }
+
+// // home.event.showNav = () => {
+// // 	var y = window.scrollY;
+// // 	if (y >= 700) {
+// // 	  	home.elem.nav.className = "nav"
+// // 		console.log(scrollY)
+// // 	}
+// // 	 else {
+// // 		home.elem.nav.className = "hide"
+// // 	}
+// // };
+
+// home.event.scrollTalk = () => {
+//     home.elem.contact.scrollIntoView({behavior: "smooth", block: "center"});
+// }
+
+// // Nav menu
+
+// // let scrollpos = window.scrollY
+// // const header = document.querySelector(".nav")
+// // const header_height = header.offsetHeight
+// // const navItem = document.querySelectorAll(".nav-item")
+
+// // function add_class_on_scroll(){
+// //     document.querySelector('.portfolio').scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+
+// //     header.querySelector('.nav-item').classList.add('active')
+// // }
+
+// // function remove_class_on_scroll(){
+// //     document.querySelector('.portfolio').scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+
+// //     header.querySelector('.nav-item').classList.contains('active').remove('active')
+// // }
+
+// // window.addEventListener('scroll', function() { 
+// //   scrollpos = window.scrollY;
+
+// //   if (scrollpos >= header_height) { add_class_on_scroll() }
+// //   else { remove_class_on_scroll() }
+
+// //   console.log(scrollpos)
+// // })
+
+// // let scrollpos = window.scrollY
+// // const header = document.querySelector(".nav")
+// // const header_height = header.offsetHeight
+
+// // const add_class_on_scroll = () => header.classList.add("fade-in")
+// // const remove_class_on_scroll = () => header.classList.remove("fade-in")
+
+// // window.addEventListener('scroll', function() { 
+// //   scrollpos = window.scrollY;
+
+// //   if (scrollpos >= header_height) { add_class_on_scroll() }
+// //   else { remove_class_on_scroll() }
+
+// //   console.log(scrollpos)
+// // })
