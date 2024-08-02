f7a60e8f484ad8639fcec71e95ccaa44a6cf5511
Brad Young <bradyoung109@gmail.com>
2024-08-02 14:51:59 +1200

    Stroke animation

--- js/home.js
+++ js/home.js
@@ -10,7 +10,6 @@ scrollBtn.addEventListener('click', function(){
     scrollDown();
 });
 
-
 // const card = document.querySelector(".card");
 
 // function handleHover(e) {
@@ -22,3 +21,83 @@ scrollBtn.addEventListener('click', function(){
 
 // card.addEventListener("mousemove", handleHover);
 // card.addEventListener("mouseleave", resetStyles);
+
+const subtitleFade1 = document.querySelector('#fadetext1');
+const subtitleFade2 = document.querySelector('#fadetext2');
+const subtitleFade3 = document.querySelector('#fadetext3');
+const subtitleFade4 = document.querySelector('#fadetext4');
+const subtitleFade5 = document.querySelector('#fadetext5');
+
+// console.log(subtitleFade1)
+
+var t1 = anime.timeline({
+    easing: 'easeInOutCubic',
+    duration: 1100,
+    loop: true,
+    autoplay: true,
+    opacity: 1,
+})
+
+t1
+.add({
+    targets: '#fadetext1',
+    opacity: 1
+}, '+=1100')
+.add({
+    targets: '#fadetext1',
+    opacity: 0
+}, '+=1100')
+.add({
+    targets: '#fadetext2',
+    opacity: 1
+}, '+=1100')
+.add({
+    targets: '#fadetext2',
+    opacity: 0
+}, '+=1100')
+.add({
+    targets: '#fadetext3',
+    opacity: 1
+}, '+=1100')
+.add({
+    targets: '#fadetext3',
+    opacity: 0
+}, '+=1100')
+.add({
+    targets: '#fadetext4',
+    opacity: 1
+}, '+=1100')
+.add({
+    targets: '#fadetext4',
+    opacity: 0
+}, '+=1100')
+.add({
+    targets: '#fadetext5',
+    opacity: 1
+}, '+=1100')
+.add({
+    targets: '#fadetext5',
+    opacity: 0
+}, '+=1100')
+.add({
+    targets: '#fadetext6',
+    opacity: 1
+}, '+=1100')
+.add({
+    targets: '#fadetext6',
+    opacity: 0
+}, '+=1100')
+
+t1.play();
+
+// var path = anime.path('.strokepath path');
+
+// anime({
+//   targets: '.strokepath .el',
+//   translateX: path('x'),
+//   translateY: path('y'),
+//   rotate: path('angle'),
+//   easing: 'linear',
+//   duration: 2000,
+//   loop: true
+// });
